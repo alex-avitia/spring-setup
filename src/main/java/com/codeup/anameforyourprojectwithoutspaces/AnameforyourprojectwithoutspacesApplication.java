0e0bf05fe3e114d22b6fd8ebdae399da9fd01e5c
Alex Avitia <alex.avitia77@gmail.com>
2021-06-10 12:03:14 -0500

    added project

---
+++ src/main/java/com/codeup/anameforyourprojectwithoutspaces/AnameforyourprojectwithoutspacesApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.anameforyourprojectwithoutspaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnameforyourprojectwithoutspacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnameforyourprojectwithoutspacesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/anameforyourprojectwithoutspaces/AnameforyourprojectwithoutspacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.anameforyourprojectwithoutspaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnameforyourprojectwithoutspacesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
