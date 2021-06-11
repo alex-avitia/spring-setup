fc2583d908a4b0223a06a306310424e1fe61b59f
Alex Avitia <alex.avitia77@gmail.com>
2021-06-11 12:19:36 -0500

    added lecture notes

--- src/main/java/com/codeup/anameforyourprojectwithoutspaces/springblog/Ad.java
+++ src/main/java/com/codeup/anameforyourprojectwithoutspaces/springblog/Ad.java
@@ -1,2 +1,20 @@
-package com.codeup.anameforyourprojectwithoutspaces.springblog;public class Ad {
+package com.codeup.anameforyourprojectwithoutspaces.springblog;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ads")
+public class Ad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //all ads have an ad.title
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    //all ads
+    @Column(nullable = false)
+    private String description;
 }
