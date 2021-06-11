e9568fd000c95449625ae135ae1ec70a5b9c3ff2
Alex Avitia <alex.avitia77@gmail.com>
2021-06-11 12:19:15 -0500

    finished dice roll exercise

---
+++ src/main/java/com/codeup/anameforyourprojectwithoutspaces/springblog/Ad.java
@@ -0,0 +1,2 @@
+package com.codeup.anameforyourprojectwithoutspaces.springblog;public class Ad {
+}
---
+++ src/main/java/com/codeup/anameforyourprojectwithoutspaces/springblog/DiceController.java
@@ -0,0 +1,25 @@
+package com.codeup.anameforyourprojectwithoutspaces.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String home() {
+        return "dice-roll";
+    }
+
+    @GetMapping("/roll-dice/{number}")
+    public String result(@PathVariable int number, Model model) {
+        int random = (int) (Math.random() * 6 + 1);
+
+        model.addAttribute("random", random);
+        model.addAttribute("number", number);
+        return "dice-result";
+    }
+
+}
